9561cd83a7f81c5632bbd8a65263f47335c235fb
priyamvadadaga <95570878+priyamvadadaga@users.noreply.github.com>
2021-12-05 23:54:25 +0400

    Add files via upload
    login now works, ui needs to be changed

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+// import 'package:flutter/material.dart';
+//
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter_signin_button/button_builder.dart';
+// import 'package:nyutrade/firebase_config.dart';
+// import 'package:firebase_core/firebase_core.dart';
+//
+// import './register_page.dart';
+// import './signin_page.dart';
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseConfig.platformOptions);
+//   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+//   runApp(AuthExampleApp());
+// }
+//
+// /// The entry point of the application.
+// ///
+// /// Returns a [MaterialApp].
+// class AuthExampleApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Firebase Example App',
+//       theme: ThemeData.dark(),
+//       home: Scaffold(
+//         body: AuthTypeSelector(),
+//       ),
+//     );
+//   }
+// }
+//
+// /// Provides a UI to select a authentication type page
+// class AuthTypeSelector extends StatelessWidget {
+//   // Navigates to a new page
+//   void _pushPage(BuildContext context, Widget page) {
+//     Navigator.of(context) /*!*/ .push(
+//       MaterialPageRoute<void>(builder: (_) => page),
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Firebase Example App'),
+//       ),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: <Widget>[
+//           Container(
+//             padding: const EdgeInsets.all(16),
+//             alignment: Alignment.center,
+//             child: SignInButtonBuilder(
+//               icon: Icons.person_add,
+//               backgroundColor: Colors.indigo,
+//               text: 'Registration',
+//               onPressed: () => _pushPage(context, RegisterPage()),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.all(16),
+//             alignment: Alignment.center,
+//             child: SignInButtonBuilder(
+//               icon: Icons.verified_user,
+//               backgroundColor: Colors.orange,
+//               text: 'Sign In',
+//               onPressed: () => _pushPage(context, SignInPage()),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
